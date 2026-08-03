08a965d8d4c838dbba1810762b8f29775092bf90
wutdahack1337 <wutdahack1337@gmail.com>
2026-08-03 18:35:49 +0700

    chore

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,9 @@ import muonsachRoutes from './routes/muonsach.routes.js';
 
 import {errorHandler} from './middleware/error.middleware.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const app = express();
 app.use(cors());
 app.use(express.json());
@@ -18,5 +21,11 @@ app.use('/api/sach', sachRoutes);
 app.use('/api/docgia', docgiaRoutes);
 app.use('/api/nhaxuatban', nhaxuatbanRoutes);
 app.use('/api/muon-sach', muonsachRoutes);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 app.use(errorHandler); // luôn đặt sau cùng
